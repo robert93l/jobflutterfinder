9c62ed2756f8e6b2c296da1023747186f74955b3
robert93l <84595455+robert93l@users.noreply.github.com>
2021-07-20 22:16:18 -0500

    Initial commit

---
+++ lib/screens/main.screen.dart
@@ -0,0 +1,191 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
+import 'package:jobflutterfinder/components/job_carousel.dart';
+import 'package:jobflutterfinder/components/job_list.dart';
+import 'package:jobflutterfinder/models/company.dart';
+import 'package:jobflutterfinder/models/job.dart';
+import '../components/job_list.dart';
+
+
+
+
+
+class MainScreen extends StatelessWidget{
+  List<Job> recentJobs = [
+    Job(
+      role: 'UX Enginner',
+      location: 'Mountain View, CA',
+      company: Company(
+        name: 'Apple',
+        urlLogo:
+        'https://i.pinimg.com/originals/1c/aa/03/1caa032c47f63d50902b9d34492e1303.jpg',
+      ),
+      isFavorite: true,
+    ),
+    Job(
+      role: 'Motion Designer',
+      location: 'New York, NY',
+      company: Company(
+        name: 'AirBnb',
+        urlLogo:
+        'https://menorcaaldia.com/wp-content/uploads/2018/02/air.jpg',
+      ),
+      isFavorite: true,
+    ),
+    Job(
+      role: 'Python Developer',
+      location: 'New York',
+      company: Company(
+        name: 'Amazon',
+        urlLogo:
+        'https://www.cbc-network.org/wp-content/uploads/2017/11/Amazon-icon.png',
+      ),
+    ),
+  ];
+
+  List<Job> forYouJobs = [
+    Job(
+      role: 'Product Designer',
+      location: 'San Francisco, CA',
+      company: Company(
+        name: 'Google',
+        urlLogo:
+        'https://images.theconversation.com/files/93616/original/image-20150902-6700-t2axrz.jpg?ixlib=rb-1.1.0&q=45&auto=format&w=1000&fit=clip',
+      ),
+    ),
+    Job(
+      role: 'Frontend Web',
+      location: 'Miami',
+      company: Company(
+        name: 'Netflix',
+        urlLogo:
+        'https://i.pinimg.com/originals/8c/74/0b/8c740bc13bd5a0a19c24d28dff98cbdd.png',
+      ),
+      isFavorite: true,
+    ),
+    Job(
+      role: 'Mobile Developer',
+      location: 'New York',
+      company: Company(
+        name: 'Amazon',
+        urlLogo:
+        'https://www.cbc-network.org/wp-content/uploads/2017/11/Amazon-icon.png',
+      ),
+    ),
+  ];
+
+    @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: SafeArea(
+        child: ListView(
+           children: <Widget>[
+             _customAppBar(),
+             _textsHeader(context),
+             _forYou(context),
+             _recent(context),
+             SizedBox(height: 50.0),
+          ],
+      ),
+    ),
+    );
+  }
+
+  Widget _customAppBar() {
+    return Padding(
+      padding: EdgeInsets.symmetric(horizontal: 15.0, vertical: 5.0),
+
+      child: Row(
+        children: <Widget>[
+          IconButton(
+            iconSize: 40.0,
+            icon: SvgPicture.asset('asset/icons/slider.svg'),
+            onPressed: (){},
+          ),
+          Column(
+            children: [
+              IconButton(
+                iconSize: 40.0,
+                icon: SvgPicture.asset('asset/icons/search.svg'),
+                onPressed: (){},
+              ),
+              IconButton(
+                iconSize: 40.0,
+                icon: SvgPicture.asset('asset/icons/settings.svg'),
+                onPressed: (){},
+              ),
+
+            ],
+          ),
+
+    ]
+    ),
+    );
+  }
+
+  Widget _textsHeader(context){
+    return Padding(
+      padding: const EdgeInsets.symmetric(vertical:20.0,horizontal:30.0),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: <Widget>[
+          Text(
+            'Hi Jade',
+            style: Theme.of(context).textTheme.bodyText1,
+          ),
+          Text(
+            'Find your next',
+            style: Theme.of(context).textTheme.headline1,
+          ),
+          Text(
+            'design job',
+            style: Theme.of(context).textTheme.headline2,
+          ),
+        ],
+      ),
+    );
+  }
+
+  Widget _forYou(context){
+    return Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: <Widget>[
+
+        Padding(
+          padding: const EdgeInsets.only(left: 30.0),
+          child: Text(
+            'For You',
+                style: Theme.of(context).textTheme.bodyText1,
+          ),
+        ),
+          JobCarousel(this.forYouJobs),
+        ]
+    );
+  }
+
+  Widget _recent(context) {
+      return Column(
+          children: <Widget>[
+           Padding(
+            padding: const EdgeInsets.only(left:30.0, right: 30.0, top:5.0, bottom:15.0),
+              child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              crossAxisAlignment: CrossAxisAlignment.end,
+                  children: <Widget>[
+                Text('Recent', style: Theme.of(context).textTheme.bodyText1,
+          ),
+
+                Text('See All', style: Theme.of(context).textTheme.bodyText2,
+          ),
+      ],
+      ),
+    ),
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 20.0),
+              child: JobList(this.recentJobs),
+            ),
+    ],
+      );
+
+  }
+}
---
+++ lib/screens/splash_screen.dart
@@ -0,0 +1,52 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+class SplashScreen extends StatelessWidget {
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Container(
+        width: double.infinity,
+        color: Colors.white,
+        child:Column(
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          children: <Widget>[
+            Image.asset('assets/imgs/img_main.png'),
+            Column(
+              children:<Widget> [
+                Text(
+                  'Job hunting',
+                    style: Theme.of(context).textTheme.headline1,
+                ),
+              ],
+            ),
+            Text(
+              'made easy',
+                style: Theme.of(context).textTheme.headline2,
+            ),
+            MaterialButton(
+              elevation: 10.0,
+              minWidth: 170.0,
+              height:50.0,
+              color: Theme.of(context).primaryColor,
+              shape: RoundedRectangleBorder(
+                borderRadius: BorderRadius.circular(8.0),
+              ),
+              child: Text(
+                'Get started',
+                style: TextStyle(
+                  color: Colors.white,
+                  fontSize: 20.0
+                ),
+              ),
+              onPressed: (){},
+            ),
+          ],
+
+
+        ),
+      ),
+    );
+  }
+}
